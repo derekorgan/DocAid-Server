5070196d8f1d8bf978b6fa10e179bb9f0bcb178b
Derek Organ <derek@1timetracking.com>
2011-11-28 13:52:40 +0000

    updated the db settings for docaid.derekorgan.com

--- www/protected/config/main.php
+++ www/protected/config/main.php
@@ -1,11 +1,11 @@
 <?php
 
 // database settings for different enviroments
-if ($_SERVER['HTTP_HOST'] == 'docaid.orchestra.io'){
-	$envhost = '10.194.111.8';
-	$envdb = 'db_cf7749e5';
-	$envuser = 'user_cf7749e5';
-	$envpass = 'kpq5Wxz9.)oijb';
+if ($_SERVER['HTTP_HOST'] == 'docaid.derekorgan.com'){
+	$envhost = 'localhost';
+	$envdb = '_docaid';
+	$envuser = 'root';
+	$envpass = 'wh1stl3r';
 
 }else {
 	$envhost = '127.0.0.1';
